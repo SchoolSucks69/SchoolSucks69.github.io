15309c07855178b51588cc7c84076dfee076367c
SchoolSucks69 <127323100+SchoolSucks69@users.noreply.github.com>
2023-05-09 11:58:36 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -8,7 +8,7 @@ addLayer("p", {
     }},
     color: "#4BDC13",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "Plank points", // Name of prestige currency
+    resource: "Plank", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
@@ -64,13 +64,13 @@ addLayer("n", {
     symbol: "NM", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
-        unlocked: true,
-		points: new Decimal(0),
+        unlocked: false,
+		Plank: new Decimal(200),
     }},
     color: "#4BDC13",
     requires: new Decimal(500), // Can be a function that takes requirement increases into account
-    resource: "Nano points", // Name of prestige currency
-    baseResource: "Plank Points", // Name of resource prestige is based on
+    resource: "Nano", // Name of prestige currency
+    baseResource: "Plank", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
