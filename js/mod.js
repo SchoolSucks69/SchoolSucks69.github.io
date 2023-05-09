d0e5edd7d91b969413239029e57df3a35c1e102f
SchoolSucks69 <127323100+SchoolSucks69@users.noreply.github.com>
2023-05-09 09:50:20 -0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -47,6 +47,8 @@ function getPointGen() {
 if (hasUpgrade('p', 11)) gain = gain.times(2)
 if (hasUpgrade('p', 12)) gain = gain.times(2)
 if (hasUpgrade('p', 13)) gain = gain.times(2.5)
+if (hasUpgrade('p', 14)) gain = gain.times(3)
+if (hasUpgrade('p', 15)) gain = gain.times(3.7)
 	return gain
 }
 
