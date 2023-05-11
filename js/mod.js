d1530f725dcc50d47c90c3b6c7085f9b4eca3162
SchoolSucks69 <127323100+SchoolSucks69@users.noreply.github.com>
2023-05-11 09:40:55 -0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -19,8 +19,8 @@ let VERSION = {
 
 let changelog = `<h1>Changelog:</h1><br>
 	<h3>v0.3</h3><br>
-		- 6 new upgrades for plank<br>
-		- 1 upgrade for nano `
+		- Added MM (Micro-Meters) <br>
+		- Upgrades: 2 Planks, 1 Nano and 1 Micro`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
